24d399c2f491f1272bb59fe5d2a5da2e257aef44
Cestnut <hunterkudo@gmail.com>
2023-09-14 17:08:49 +0200

    fixed, now adds trailing zero

--- common/common.c
+++ common/common.c
@@ -18,13 +18,14 @@ int is_number(char *string){
 }
 
 char *random_string(int bytes_length){
-    
-    char *output_string = malloc(bytes_length * sizeof(char));
+    //Adds 1 element to account for trailing zero
+    char *output_string = malloc((bytes_length * sizeof(char)) + 1);
 
     for(int i = 0; i < bytes_length; i++){
         //Prints to every character and hex value between 0x0 and 0xF
         sprintf(&output_string[i], "%hhX", rand()%16);
     }
+    output_string[bytes_length] = 0x0;
 
     return output_string;
 }
