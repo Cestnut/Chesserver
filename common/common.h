4d010f62e0520529a7d2db5ef5ae3452598699d0
Cestnut <hunterkudo@gmail.com>
2023-09-21 14:55:15 +0200

    added new message types

--- common/common.h
+++ common/common.h
@@ -9,8 +9,10 @@
 
 //enum begins from 1 because values are casted from string with atoi(). 
 //atoi returns 0 when an invalid character is met.
-typedef enum {CREATE_GAME=1, JOIN_GAME, EXIT} client_choice;
 
+typedef enum {RUNNING=1, CHECKMATE, STALEMATE, TIMEOUT} game_status;
+typedef enum {CREATE_GAME=1, JOIN_GAME, EXIT} client_choice;
+typedef enum {INVALID_MOVE=1, VALID_MOVE} move_validation_result;
 typedef enum{
     NO_ERROR=0,
     GAME_NAME_TAKEN,
