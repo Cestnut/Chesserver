8901871fbec0b4c788d43912be2bf1cb024599e5
Cestnut <hunterkudo@gmail.com>
2023-09-21 14:54:22 +0200

    added game_menu, still have to be tested

--- client/client.c
+++ client/client.c
@@ -1,6 +1,7 @@
 #include "connection.h"
 #include <string.h>
 #include <unistd.h>
+#include "../common/chess.h"
 
 error create_game(int client_fd);
 error join_game(int client_fd);
@@ -70,7 +71,7 @@ int main(int argc, char **argv){
                 error_code = create_game(client_fd);
                 if(error_code==NO_ERROR){
                     printf("Game created and joined\n");
-                    game_menu();
+                    game_menu(client_fd);
                 }
                 break;
                     
@@ -78,7 +79,7 @@ int main(int argc, char **argv){
                 error_code = join_game(client_fd);
                 if(error_code==NO_ERROR){
                     printf("Game joined\n");
-                    game_menu();
+                    game_menu(client_fd);
                 }
                 break;
 
@@ -113,7 +114,7 @@ int main(int argc, char **argv){
 }
 
 error create_game(int client_fd){
-    char input_buffer[64];
+    char input_buffer[BUFFER_LEN];
     error error_code;
     puts("Insert game name, timer length in the form: <GAME_NAME>:<TIMER>");
     fgets(input_buffer, sizeof(input_buffer), stdin);
@@ -136,6 +137,76 @@ error join_game(int client_fd){
     return error_code;
 }
 
-void game_menu(){
+void game_menu(int client_fd){
+    char input_buffer[GAME_NAME_MAX_LENGTH];
+    char player_color_str[10];
+    Position positions[2];
+    piece_color player_color, curr_color=WHITE;
+    game_status status = RUNNING;
+    board_struct *board;
+    move_validation_result server_response_move;
 
+    printf("Waiting for game to begin\n");
+    recv(client_fd, &player_color, sizeof(player_color), 0);
+    if(player_color == WHITE){
+        strncpy(player_color_str, "WHITE", sizeof(player_color_str));
+    }
+    else if(player_color == BLACK){
+        strncpy(player_color_str, "BLACK", sizeof(player_color_str));
+    }
+
+    board = init_board();
+
+    printf("The game has begun! You're %s\n", player_color_str);
+    while(status == RUNNING){
+        render_board(board, player_color);
+        if(curr_color == player_color){
+            printf("It's your turn!\n");
+            while(TRUE){
+                fgets(input_buffer, sizeof(input_buffer), stdin);
+                if(parse_move(positions, input_buffer) == NULL){
+                    printf("Invalid input\n");
+                }
+                else if(!is_move_valid(board, player_color, positions[0], positions[1])){
+                    printf("Mossa invalida\n");
+                }
+                else{
+                    //Sends the move and waits for feedback
+                    send(client_fd, input_buffer, strlen(input_buffer), 0);
+                    recv(client_fd, &server_response_move, sizeof(server_response_move), 0);
+                    if(server_response_move == VALID_MOVE){
+                        printf("%d%d to %d%d\n", positions[0].col, positions[0].row, positions[1].col, positions[1].row);
+                        move_piece(board, positions[0], positions[1]);
+                        break;
+                    }
+                }
+            }   
+        }
+        else{
+            printf("It's your opponent's turn!\n");
+            recv(client_fd, &input_buffer, sizeof(input_buffer), 0);
+            parse_move(positions, input_buffer);
+            move_piece(board, positions[0], positions[1]);
+        }
+        
+
+        //Change current color
+        if(curr_color == WHITE) curr_color = BLACK;
+        else if(curr_color == BLACK) curr_color = WHITE;
+        
+        recv(client_fd, &status, sizeof(status), 0);
+
+    }
+    switch(status){
+        case RUNNING:
+            break;
+        case CHECKMATE:
+            break;
+        case STALEMATE:
+            break;
+        case TIMEOUT:
+            break;
+        default:
+            break;
+    }
 }
