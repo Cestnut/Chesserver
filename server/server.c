a03c2fc6cd4a94a0d616af777ce5af29eea693be
Cestnut <hunterkudo@gmail.com>
2023-09-26 14:32:03 +0200

    removed useless stuff and handling SIGPIPE with SIGN_IGN

--- server/server.c
+++ server/server.c
@@ -1,10 +1,9 @@
 #include "server_connection.h"
 #include "game_handling.h"
-
+#include <signal.h>
+    
 
 int main(int argc, char **argv){
-
-    srand(time(NULL));
 
     int server_fd, client_fd;
     int port = 5555;
@@ -12,11 +11,11 @@ int main(int argc, char **argv){
     struct sockaddr_in client_addr;
     socklen_t client_addr_len;
 
-    char input_buffer[BUFFER_LEN];
-    
-    memset(input_buffer, 0, BUFFER_LEN);
-
     server_fd = create_server_socket(port);
+    
+    //Ignores SIGPIPE signal
+    sigaction(SIGPIPE, &(struct sigaction){{SIG_IGN}}, NULL);
+    srand(time(NULL));
     init_games();
 
     pthread_t tid;
@@ -35,50 +34,8 @@ int main(int argc, char **argv){
 
         pthread_create(&tid, NULL, client_worker, (void *)worker_args);    
     }
-        int nfds = 1, ready;
-        struct pollfd *pfds;
-
-        pfds = calloc(nfds, sizeof(struct pollfd));
-        pfds[0].fd = client_fd;
-        pfds[0].events = POLLIN;
-        pfds[0].revents = POLLIN;
-        ready = poll(pfds, nfds, 3000);
-
-        if(ready > 0){
-            recvline(client_fd, input_buffer, sizeof(input_buffer), 0);
-            send(client_fd, input_buffer, strlen(input_buffer), 0);
-        }
-        else if(ready == 0){
-            printf("Timeout\n");
-        }
-        else{
-            printf("Error polling client_fd: errno %d\n", errno);
-            exit(0);
-        }
-    
-    close(client_fd);
+        
     close(server_fd);
 
     return 0;
-    
-    
-    }
-
-/*
-1: create socket
-2: set port and IP
-3: bind to the set port and ip
-4: listen for clients
-5: accept incoming connection
-6: Receive client's message and send response
-7: close socket
-
-
-sockaddr_in.sin_family
-sockaddr_in.sin_port
-sockaddr_in.sin_addr.s_addr
-listen
-accept
-recv
-send
-*/
+}
